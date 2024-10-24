34edb4001585a15cc6a0fe8756a6dfbd25b10189
Sedona Psiloyenis <77178501@edu.cbe.ab.ca>
2024-10-24 13:57:28 -0600

    allowed to manually set answer and worked on guess endpoint

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ let activeSessions = {};
 server.get("/newgame", (req, res) => {
   //generates new ID
   let newID = uuid.v4();
-  console.log(newID);
 
   let newGame = {
     wordToGuess: "mouse",
@@ -31,6 +30,12 @@ server.get("/newgame", (req, res) => {
     gameOver: false,
   };
 
+  //set word manually
+  if (req.query.answer) {
+    console.log("manually set answer");
+    newGame.wordToGuess = req.query.answer;
+  }
+
   activeSessions[newID] = newGame;
 
   res.status(201);
@@ -38,7 +43,7 @@ server.get("/newgame", (req, res) => {
 });
 
 server.get("/gamestate", (req, res) => {
-  console.log(req.query.sessionID);
+  //console.log(req.query.sessionID);
 
   res.send({ gameState: activeSessions[req.query.sessionID] });
   res.status(200);
@@ -48,7 +53,19 @@ server.post("/guess", (req, res) => {
   let guess = req.body.guess;
   let sessionID = req.body.sessionID;
 
+  //checks if guess only contains letters
+  let onlyLetters = /^[A-Z]+$/i.test(guess);
+
+  //turns string into array
+  let myArr = guess.split("");
+  console.log(myArr);
+
+  if (guess.length == 5 && onlyLetters) {
+    console.log("guess is  5 characters and only letters");
+  }
+
   res.status(201);
+  res.send();
 });
 
 //DO NOT DELETE
